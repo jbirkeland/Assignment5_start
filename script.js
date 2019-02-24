b0aa1ffa49dc6ba5e8db30440273efb8a8046acd
jordan birkeland <jbirkeland@mcad.edu>
2019-02-24 13:29:57 -0600

    Added 'donate' button JS

--- script.js
+++ script.js
@@ -19,17 +19,37 @@ var churchillSpeech = {
     },
     speechesArray = [churchillSpeech, ghandiSpeech, demosthenesSpeech];
 
-var userName = window.prompt("Enter your name, then click OK");
+var user = window.prompt("Enter your name, then click OK");
 
-if (userName === "" || userName === null) {
+if (user === "" || user === null) {
   console.log ("Ok, I'll just call you User.");
-} else if (userName !== "") {
-  console.log ("Hi " + userName + "!");
-} 
-     
+} else if (user !== "") {
+  console.log ("Hi " + user + "!");
+}
+
 
 document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
+  
+  var favoriteAuthor = window.prompt("Enter the name of your favorite author - Churchill, Ghandi, Demosthenes - then click OK");
+
+  switch (favoriteAuthor) {
+    case "Churchill":
+    console.log (speechesArray[0].author + " was " + speechesArray[0].authorAge + " during this speech.");
+    break;
+
+    case "Ghandi":
+    console.log (speechesArray[1].author + " was " + speechesArray[1].authorAge + " during this speech.");
+    break;
+
+    case "Demosthenes":
+    console.log (speechesArray[2].author + " was " + speechesArray[2].authorAge + " during this speech.");
+    break;
+
+    default:
+    console.log ("You did not enter one of the author's names correctly.");
+    break;
+  }
 });
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
